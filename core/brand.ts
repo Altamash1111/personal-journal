e967da1a5a928e77f2a470eede8523ea2cd31485
Altamash1111 <altamshkhan56789@gmail.com>
2026-09-01 13:18:04 +0530

    Delete core directory

--- core/brand.ts
+++
@@ -1,4 +0,0 @@
-/** Nominal typing helper. `Brand<string, \"GoalId\">` is assignment-incompatible
- *  with a raw string or another brand, catching id/date mix-ups at compile time. */
-declare const __brand: unique symbol;
-export type Brand<T, B extends string> = T & { readonly [__brand]: B };
